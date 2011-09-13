613333fde622541027ec2669d65fc702517ac3f0
104481 <u63742845@false.(none)>
2011-09-13 16:33:16 -0400

    fixes for server version

---
+++ public/application/views/media/none_body.php
@@ -0,0 +1,22 @@
+<div id="left">
+ <h2>
+   Archive
+    </h2>
+     <p>
+       Search capabilities are coming soon.
+        </p>
+	</div>
+
+	<div id="right">
+	 <h1>
+	   Messages by date
+	    </h1>
+	     <p>
+	         &nbsp;
+		  </p>
+		       
+			   <p>
+			   There are currently no messages to display.
+			   </p>
+			    </div>
+			    
