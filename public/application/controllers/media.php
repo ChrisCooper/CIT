86607c8e3da8bbe5bb14fba64eb483ad5f57e1b1
Chris Cooper <chriscooper1991@gmail.com>
2011-11-03 18:36:37 -0400

    Date picker now always appears on the search page.

--- public/application/controllers/media.php
+++ public/application/controllers/media.php
@@ -60,8 +60,8 @@ class Media extends CI_Controller {
                 redirect('/media/item/', 'refresh');
             } else {
 		
-		$view_info = array();
-		$view_info['options'] = $this->_get_search_dropdown_array();
+				$view_info = array();
+				$view_info['options'] = $this->_get_search_dropdown_array();
 		
                 $this->template->write_view('body', 'media/none_body', $view_info);
                 $this->template->render();
@@ -80,7 +80,7 @@ class Media extends CI_Controller {
             $message_markup = "";
             
             foreach ($messages as $message) {
-		$message_series = $this->message->fetch_series_filename_and_title($message->series_id);
+				$message_series = $this->message->fetch_series_filename_and_title($message->series_id);
                 $message->series_filename = $message_series->filename;
                 $message->title = $message_series->title . " - " . $message->title;
                 $message_markup .= $this->load->view('media/individual_message', $message, true);
@@ -99,25 +99,25 @@ class Media extends CI_Controller {
     
     function search($starting_number=0){
 	
-	if (sizeof($_GET) == 0) {
-		redirect('/media/item/', 'refresh');
-	}
+		if (sizeof($_GET) == 0) {
+				redirect('/media/item/', 'refresh');
+		}
 	
-	if ($starting_number < 0){
-            redirect('/media/item/', 'refresh');
-        }
-	
-	
-	//echo "?" . http_build_query($_GET, '', "&");
-	
-	//return;
+		if ($starting_number < 0){
+				redirect('/media/item/', 'refresh');
+		}
 	
 	
-	$this->_write_resources();
+		//echo "?" . http_build_query($_GET, '', "&");
+	
+		//return;
+	
+	
+		$this->_write_resources();
         
-        $this->template->write('title_addition', 'Media - Messages');
+		$this->template->write('title_addition', 'Media - Messages');
         
-        $messages = $this->message->get_ten_search_results_from_offset($starting_number - 1);
+		$messages = $this->message->get_ten_search_results_from_offset($starting_number - 1);
         
         if ($messages === FALSE){
             
@@ -125,73 +125,75 @@ class Media extends CI_Controller {
                 redirect('/media/item/', 'refresh');
             } else {
 		
-		$view_info = array();
+				$view_info = array();
 		
-		$all_series = $this->message_series_model->get_dropdown_info();
+				$all_series = $this->message_series_model->get_dropdown_info();
             
-		$view_info['options'] = $this->_get_search_dropdown_array();
+				$view_info['options'] = $this->_get_search_dropdown_array();
 		
                 $this->template->write_view('body', 'media/search/none_body', $view_info);
                 $this->template->render();
             }
             
         } else {
-            $this->load->library('pagination');
-
-            $config['base_url'] = site_url("media/search/");
-            $config['total_rows'] = $this->message->count_search_results();
-            $config['per_page'] = 10;
-            $config['num_links'] = 7;
-	    //Add these for jquery GET script
-	    $config['full_tag_open'] = '<div id="pagination">';
-            $config['full_tag_close'] = '</div>';
-        
-            $this->pagination->initialize($config);
-	    
-	    $scripts = '<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>';
-	    $scripts .= "<script>window.onload=function(){
-	    console.log('going');
-	    $('#pagination > a').each(function() {
-    var g = window.location.href.slice(window.location.href.indexOf('?'));
-    var href = $(this).attr('href');
-    $(this).attr('href', href+g);
-});
-}
-</script>";
-	    $this->template->write('_scripts', $scripts);
-            
-            $message_markup = "";
-            
-            foreach ($messages as $message) {
-		$message_series = $this->message->fetch_series_filename_and_title($message->series_id);
-                $message->series_filename = $message_series->filename;
-                $message->title = $message_series->title . " - " . $message->title;
-                $message_markup .= $this->load->view('media/individual_message', $message, true);
-            }
-            
-            $view_info['messages'] = $message_markup;
-            $view_info['pagination_links'] = $this->pagination->create_links();
-            
-	    $view_info['options'] = $this->_get_search_dropdown_array();
+				$this->load->library('pagination');
 	
-            $this->template->write_view('body', 'media/body', $view_info);
+				$config['base_url'] = site_url("media/search/");
+				$config['total_rows'] = $this->message->count_search_results();
+				$config['per_page'] = 10;
+				$config['num_links'] = 7;
+				//Add these for jquery GET script
+				$config['full_tag_open'] = '<div id="pagination">';
+				$config['full_tag_close'] = '</div>';
+			
+				$this->pagination->initialize($config);
+	    
+				$scripts = "<script>
+				
+				window.onload = function (){
+						$('#pagination > a').each(function() {
+								var g = window.location.href.slice(window.location.href.indexOf('?'));
+								var href = $(this).attr('href');
+								$(this).attr('href', href+g);
+						});
+				}
+				
+				</script>";
+				
+				$this->template->write('_scripts', $scripts);
             
-            $this->template->render();
+				$message_markup = "";
+            
+				foreach ($messages as $message) {
+						$message_series = $this->message->fetch_series_filename_and_title($message->series_id);
+						$message->series_filename = $message_series->filename;
+						$message->title = $message_series->title . " - " . $message->title;
+						$message_markup .= $this->load->view('media/individual_message', $message, true);
+				}
+            
+				$view_info['messages'] = $message_markup;
+				$view_info['pagination_links'] = $this->pagination->create_links();
+            
+				$view_info['options'] = $this->_get_search_dropdown_array();
+	
+				$this->template->write_view('body', 'media/body', $view_info);
+				
+				$this->template->render();
         }
     }
     
     function _get_search_dropdown_array(){
-	$all_series = $this->message_series_model->get_dropdown_info();
-            
-	$array = array();
-	    
-	foreach ($all_series as $series) {
-		$array[$series->id] = $series->title;
-        }
-	
-	$array[''] = 'Any';
-	
-	return $array;
+		$all_series = $this->message_series_model->get_dropdown_info();
+				
+		$array = array();
+			
+		foreach ($all_series as $series) {
+				$array[$series->id] = $series->title;
+		}
+		
+		$array[''] = 'Any';
+		
+		return $array;
     }
     
     function listen($message_id) {
